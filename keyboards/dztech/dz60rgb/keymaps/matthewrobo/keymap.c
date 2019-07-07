995eaa148b459555df532166c5f12a8057085498
Nico <me@nico.news>
2019-07-07 21:39:51 +0900

    ad dz60

--- keyboards/dztech/dz60rgb/keymaps/matthewrobo/keymap.c
+++ keyboards/dztech/dz60rgb/keymaps/matthewrobo/keymap.c
@@ -32,7 +32,7 @@ extern bool autoshift_enabled;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_QWERTY] = LAYOUT(
-		KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, \
+		KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    ,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, \
 		KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, \
 		LT_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,           \
 		KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  MT_SLSH,          KC_UP,   LT_DEL,  \
@@ -175,7 +175,7 @@ void rgb_matrix_indicators_user(void)
 			rgb_matrix_set_color(52, 0xFF, 0x40, 0x00); //TOG
 			rgb_matrix_set_color(25, 0x80, 0x80, 0x80); //SPI
 			rgb_matrix_set_color(38, 0x80, 0x80, 0x80); //SPD
-			rgb_matrix_set_color(24, rgbHUI.r, rgbHUI.g, rgbHUI.b); //HUI
+			rgb_matrix_set_color(''24'', rgbHUI.r, rgbHUI.g, rgbHUI.b); //HUI
 			rgb_matrix_set_color(37, rgbHUD.r, rgbHUD.g, rgbHUD.b); //HUD
 			rgb_matrix_set_color(23, rgbSAI.r, rgbSAI.g, rgbSAI.b); //SAI
 			rgb_matrix_set_color(36, rgbSAD.r, rgbSAD.g, rgbSAD.b); //SAD
